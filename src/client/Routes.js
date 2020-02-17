79329a0be6f6efa5597a4b22b05dd15eb56f281b
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-02-17 00:04:46 +0000

    Created basic home route along with header and footer files.
    Now ready to start the authentication branch

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 
+// Importing the pages
+import Home from './website_pages/Home';
+
 function Routes() {
   return (
     <Router>
-      <Switch></Switch>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </Router>
   );
 }
