1939e1652fb8550aa1ddfd67d843e6bcbbc097be
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-03-08 12:43:58 +0000

    Implemented checks to make sure the company subdir matches and also the application is in a authenticated state.

--- src/client/routes/Guarded.js
+++ src/client/routes/Guarded.js
@@ -1,8 +1,31 @@
 import React, { Component } from "react";
+import Cookies from "js-cookie";
+
+import Error401 from "../website_pages/error_pages/Error401";
 
 class Guarded extends Component {
+  state = {
+    allowed: false
+  };
+
+  componentDidMount() {
+    const storedSubdir = Cookies.get("auth-company-subdir");
+    if (this.props.match.params.company_subdir === storedSubdir) {
+      this.setState({
+        allowed: true
+      });
+    }
+  }
   render() {
-    return <this.props.page {...this.props} />;
+    return (
+      <div>
+        {this.state.allowed && this.props.authenticated ? (
+          <this.props.page {...this.props} />
+        ) : (
+          <Error401 />
+        )}
+      </div>
+    );
   }
 }
 
