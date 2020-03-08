dae8443b0b01a7489f8a8bb263e0a4c6080fd5e5
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-03-08 13:38:33 +0000

    Completed implementation of guarded route.
    Guarded route has multiple fallbacks now.
    It will either
    - Return the inteded page
    - If the app is in authenticated state then it will show a page where the user can choose to either log in to attempted help desk, or redirect to the help desk they are logged in to
    - Finally show a 401 error page.

--- src/client/routes/Guarded.js
+++ src/client/routes/Guarded.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
 import Cookies from "js-cookie";
 
-import Error401 from "../website_pages/error_pages/Error401";
+import { Redirect } from "react-router-dom";
+
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography
+} from "@material-ui/core";
+
+import Error404 from "../website_pages/error_pages/Error404";
 
 class Guarded extends Component {
   state = {
-    allowed: false
+    allowed: false,
+    redirectToThisHelpDesk: false
+  };
+
+  yourHelpDeskHref = "/" + Cookies.get("auth-company-subdir") + "/dashboard";
+  loginToThisHelpDeskHref = "/" + this.props.match.params.company_subdir + "/";
+
+  loginToThisHelpDesk = () => {
+    Cookies.remove("token");
+    Cookies.remove("token-type");
+    Cookies.remove("auth-company-subdir");
+    this.setState({
+      redirectToThisHelpDesk: true
+    });
+    this.props.setAppState("authenticated", false);
   };
 
   componentDidMount() {
@@ -17,12 +41,53 @@ class Guarded extends Component {
     }
   }
   render() {
+    var cardStyle = {
+      maxWidth: "300px",
+      marginLeft: "auto",
+      marginRight: "auto"
+    };
+
     return (
       <div>
         {this.state.allowed && this.props.authenticated ? (
           <this.props.page {...this.props} />
+        ) : this.props.authenticated ? (
+          <div className="flex-center">
+            <Card style={cardStyle} variant="outlined">
+              <CardContent>
+                <Typography variant="h4" component="h1" gutterBottom>
+                  Uh oh.
+                </Typography>
+                <Typography component="p">
+                  Looks like you got lost while trying to find your help desk.
+                  Buuut just in case this was intentional, feel free to choose
+                  from the options below.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  href={this.yourHelpDeskHref}
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  style={{ textAlign: "center" }}
+                >
+                  Go To Your Help Desk
+                </Button>
+                <Button
+                  onClick={this.loginToThisHelpDesk}
+                  variant="contained"
+                  size="small"
+                >
+                  Log In To This Help Desk
+                </Button>
+              </CardActions>
+            </Card>
+          </div>
+        ) : this.state.redirectToThisHelpDesk ? (
+          <Redirect to={this.loginToThisHelpDeskHref} />
         ) : (
-          <Error401 />
+          <Error404 />
         )}
       </div>
     );
