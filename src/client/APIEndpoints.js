d374c0d4d50473b23734087200deded1b01b98f9
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-03-27 22:35:29 +0000

    Updated APIEndpoints.js
    Refactored APIEndpoints to use generatePath.
    Currently no pages using APIEndpoint works.

--- src/client/APIEndpoints.js
+++ src/client/APIEndpoints.js
@@ -1,37 +1,42 @@
 import AppConfig from "./AppConfig";
+import { generatePath } from "react-router-dom";
 
 const APIEndpoints = {
   endpoints: {
-    login: "/api/login",
-    checkToken: "/api/check-token",
+    login: ":company_subdir/api/login",
+    checkToken: ":company_subdir/api/check-token",
 
-    createUser: "/api/users/create",
-    updateUser: "/api/users/update",
-    toggleActiveUser: "/api/users/toggleActive",
-    getAllUsers: "/api/users/get/all",
-    getSingleUser: "/api/users/get/",
+    createUser: ":company_subdir/api/users/create",
+    updateUser: ":company_subdir/api/users/update",
+    toggleActiveUser: ":company_subdir/api/users/toggleActive",
+    getAllUsers: ":company_subdir/api/users/get/all",
+    getSingleUser: ":company_subdir/api/users/get/:id",
 
-    createRole: "/api/roles/create",
-    updateRole: "/api/roles/update",
-    deleteRole: "/api/roles/delete",
-    getAllRoles: "/api/roles/get/all",
-    getSingleRole: "/api/roles/get/",
+    createRole: ":company_subdir/api/roles/create",
+    updateRole: ":company_subdir/api/roles/update",
+    deleteRole: ":company_subdir/api/roles/delete",
+    getAllRoles: ":company_subdir/api/roles/get/all",
+    getSingleRole: ":company_subdir/api/roles/get/:id",
 
-    createClient: "/api/clients/create",
-    updateClient: "/api/clients/up date",
-    deleteClient: "/api/clients/delete",
-    getAllClients: "/api/clients/get/all",
-    getSingleClient: "/api/clients/get/",
+    createClient: ":company_subdir/api/clients/create",
+    updateClient: ":company_subdir/api/clients/up date",
+    deleteClient: ":company_subdir/api/clients/delete",
+    getAllClients: ":company_subdir/api/clients/get/all",
+    getSingleClient: ":company_subdir/api/clients/get/:id",
 
-    createCall: "/api/calls/create",
-    updateCall: "/api/calls/update",
-    deleteCall: "/api/calls/delete",
-    getAllCall: "/api/calls/get/all",
-    getSingleCall: "/api/calls/get/"
+    createCall: ":company_subdir/api/calls/create",
+    updateCall: ":company_subdir/api/calls/update",
+    deleteCall: ":company_subdir/api/calls/delete",
+    getAllCall: ":company_subdir/api/calls/get/all",
+    getSingleCall: ":company_subdir/api/calls/get/:id"
   },
 
-  get: function(endpointName, companySubdir) {
-    return AppConfig.API_URL + companySubdir + this.endpoints[endpointName];
+  get: function(endpointName, data) {
+    if (typeof data !== "object")
+      throw new Error("Data is a non-object when object is required.");
+    if (typeof data.company_subdir === "undefined")
+      throw new Error("company_subdir was not defined in data.");
+    generatePath(AppConfig.API_URL + this.endpoints[endpointName], data);
   }
 };
 
