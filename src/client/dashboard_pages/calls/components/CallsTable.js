7f9a2a65b0c4cf90235803c574809122065418a7
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-05-06 21:17:54 +0100

    Implemented basic filters for sorting calls and styles for that.

--- src/client/dashboard_pages/calls/components/CallsTable.js
+++ src/client/dashboard_pages/calls/components/CallsTable.js
@@ -16,6 +16,9 @@ import {
   Paper,
   Button,
   Box,
+  Select,
+  MenuItem,
+  Divider,
 } from "@material-ui/core";
 
 class CallsTable extends Component {
@@ -24,13 +27,27 @@ class CallsTable extends Component {
     calls: {},
     prevPageURL: null,
     nextPageURL: null,
+    handledBy: "self",
+    createdAt: "asc",
+    resolved: "no",
   };
 
-  componentDidMount() {
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  getCalls = (e) => {
     const getAllCallsEndpoint = Endpoints.get("api", "getAllCalls", {
       company_subdir: this.company_subdir,
     });
-    const headers = getBaseHeaders();
+    let headers = getBaseHeaders();
+    headers.params = {
+      handled_by: this.state.handledBy,
+      created_at: this.state.createdAt,
+      resolved: this.state.resolved,
+    };
     axios
       .get(getAllCallsEndpoint, headers)
       .then((res) => {
@@ -51,10 +68,19 @@ class CallsTable extends Component {
           }
         }
       });
+  };
+
+  componentDidMount() {
+    this.getCalls();
   }
 
   loadNewCalls = (direction) => {
     let headers = getBaseHeaders();
+    headers.params = {
+      handled_by: this.state.handledBy,
+      created_at: this.state.createdAt,
+      resolved: this.state.resolved,
+    };
     let endpoint = this.state.nextPageURL;
     if (direction === "previous") {
       endpoint = this.state.prevPageURL;
@@ -81,11 +107,114 @@ class CallsTable extends Component {
       });
   };
 
+  submitFilters = (e) => {
+    e.preventDefault();
+    this.getCalls();
+  };
+
+  resetFilters = () => {
+    this.setState({
+      resolved: "",
+      createdAt: "",
+      handledBy: "",
+    });
+  };
+
   render() {
     const { company_subdir } = { ...this.props };
-    const { calls, prevPageURL, nextPageURL } = { ...this.state };
+    const {
+      calls,
+      handledBy,
+      createdAt,
+      resolved,
+      prevPageURL,
+      nextPageURL,
+    } = {
+      ...this.state,
+    };
     return (
       <div>
+        <Box
+          component="form"
+          onSubmit={this.submitFilters}
+          display="flex"
+          flexDirection="row"
+          flexWrap="wrap"
+          justifyContent="space-between"
+          className="xs-full-width"
+        >
+          <Select
+            name="handledBy"
+            value={handledBy}
+            className="xs-full-width md-fifth-width standard-margin-bottom"
+            onChange={this.onChange}
+            displayEmpty
+          >
+            <MenuItem key={0} value="">
+              Handled By
+            </MenuItem>
+            <Divider />
+            <MenuItem key={1} value="self">
+              Your Calls
+            </MenuItem>
+            <MenuItem key={2} value="all">
+              All Calls
+            </MenuItem>
+          </Select>
+          <Select
+            name="resolved"
+            value={resolved}
+            className="xs-full-width md-fifth-width standard-margin-bottom"
+            onChange={this.onChange}
+            displayEmpty
+          >
+            <MenuItem key={0} value="">
+              Resolved
+            </MenuItem>
+            <Divider />
+            <MenuItem key={1} value="yes">
+              Resolved
+            </MenuItem>
+            <MenuItem key={2} value="no">
+              Not Resolved
+            </MenuItem>
+          </Select>
+          <Select
+            name="createdAt"
+            value={createdAt}
+            className="xs-full-width md-fifth-width standard-margin-bottom"
+            onChange={this.onChange}
+            displayEmpty
+          >
+            <MenuItem key={0} value="">
+              Created At
+            </MenuItem>
+            <Divider />
+            <MenuItem key={1} value="asc">
+              Oldest
+            </MenuItem>
+            <MenuItem key={2} value="desc">
+              Latest
+            </MenuItem>
+          </Select>
+          <Button
+            type="submit"
+            variant="contained"
+            color="secondary"
+            className="xs-full-width md-fifth-width standard-margin-bottom"
+          >
+            Filter
+          </Button>
+          <Button
+            type="button"
+            variant="contained"
+            color="primary"
+            className="xs-full-width md-fifth-width standard-margin-bottom"
+            onClick={this.resetFilters}
+          >
+            Reset
+          </Button>
+        </Box>
         <TableContainer component={Paper} className="standard-margin-bottom">
           <Table aria-label="table containing the calls">
             <TableHead>
