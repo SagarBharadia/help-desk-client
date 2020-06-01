6c36af0718a30c8ec54f4de08e4e7f5fcc5f4ab0
Sagar Bharadia <sagarbharadia@hotmail.com>
2020-06-01 07:22:48 +0100

    Updated message to users for the dashboard

--- src/client/dashboard_pages/Dashboard.js
+++ src/client/dashboard_pages/Dashboard.js
@@ -10,6 +10,10 @@ class Dashboard extends Component {
       <DashboardWrapper {...this.props}>
         <main>
           <p>Welcome to the Help Desk Application for {companyName}.</p>
+          <p>
+            To get started press the hamburger button in the top left to see the
+            areas!
+          </p>
         </main>
       </DashboardWrapper>
     );
